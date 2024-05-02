a2ad71098ce355dcf4cfd1a909dc6b4d83cb611f
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-05-02 21:54:35 +0500

    Structe of Project

---
+++ src/components/extras/index.js
@@ -0,0 +1,17 @@
+import Logo from "./Logo";
+import Box from "./Box";
+import Button from "./Button";
+import Container from "./Container";
+import Card from "./Card";
+import Input from "./Input";
+import Select from "./Select";
+
+export {
+    Logo,
+    Box,
+    Button,
+    Container,
+    Card,
+    Input,
+    Select
+}
