0749b4cf3d96d460fd3e475c1100a69318e58d95
Aishakz <aishakzkhanzada378@gmail.com>
2024-05-08 10:01:46 +0500

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Home, About, Contact, Gallery, Blog} from "./components/index"
+import Container from './components/extras/Container'
 
 function App() {
   return (
